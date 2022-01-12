82bf70f57bb60b80adbcf9f1ce109682d1e39bda
Joel Nilsson <joel.j.nilsson@afry.com>
2022-01-12 16:06:28 +0100

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,6 +4,8 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
+#Are you kidding ??? What the **** are you talking about man ? You are a biggest looser i ever seen in my life ! You was doing PIPI in your pampers when i was beating players much more stronger then you!
+
 
 
 app = FastAPI(title="Predicting Wine Class with batching")
